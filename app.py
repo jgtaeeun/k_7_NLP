11a3d14c7543530de803572c7cec1b0d7a5cce00
jgtaeeun <jas49258667@gmail.com>
2024-07-26 11:09:41 +0900

    0726 템플릿 {{ 변수}} , {% %} 지시연산을 통한 html 상속, url_for(")

--- app.py
+++ app.py
@@ -31,18 +31,25 @@ def nlp_predict():
     if request.method== "GET":
        return render_template("predict.html")
     else :
-        review=request.form["review"]
-        print(review)  
-        return render_template("predict_result.html", review=review)  
+        _review=request.form["review"]  
+        _review_matrix=tfidf_vector.transform([ _review])
+        _review_result=model_lf.predict(_review_matrix)[0]
+        _review_result="긍정" if _review_result else "부정"
+        result={
+           "review": _review,
+           "review_result":_review_result
+        }
+        return render_template("predict_result.html", result=result)  
 
 @app.route("/")
 def index():
-    #해당 테스트 코드는 잘 작동하지만 별도의 함수로 빠져나가야 됨
-    test_str="이 영화 재미있어요! 하하하!"
-    test_matrix=tfidf_vector.transform([test_str])
-
-    result=model_lf.predict(test_matrix)
-    print(result)
+    
+    # 해당 테스트 코드는 잘 작동하지만
+    # 별도의 함수로 빠져나가야 됨
+   # test_str = "이 영화 재미있어요! 하하하"
+   # test_matrix = lt_transform(test_str)
+   # result = model_lr.predict(test_matrix)
+   # print(result)
     return render_template("index.html")
 
 
